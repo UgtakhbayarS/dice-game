8a61d304f84aa04753dc686b2f4d758950a602a0
ukasan <ugtakhbayar0217@gmail.com>
2019-11-28 21:27:43 +0800

    roll dice shoog shiddeg bolloo

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var scores = [0, 0];
 //toglogchiin eeljindee tsugluulgh onoo
 var roundScore = 0;
 //shoonii buusan too 1 - 6 random uusgej ugnu
-var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-console.log("Шоо: " + dice);
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+  //   alert("Шоо буулаа : " + diceNumber);
+});
